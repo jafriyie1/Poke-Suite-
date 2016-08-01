9fb60570410125337162cc23fca9df727b68d76a
jafriyie1 <sfjafthemp@gmail.com>
2016-08-01 17:46:33 -0500

    New additions

--- Phase 3/poke_cluster.py
+++ Phase 3/poke_cluster.py
@@ -1,5 +1,6 @@
 '''Unsupervised learning analysis on Twitter Data'''
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import cluster
 import pandas as pd
 
 df = pd.read_json("/Users/Joel/Desktop/Tweets/final_poke_tweets.json")
@@ -7,4 +8,4 @@ text_data = df["text"].fillna('')
 
 count_vect = CountVectorizer()
 final_text_data = count_vect.fit_transform(text_data)
-count_vect.vocabulary_.get('Pokemon')
+print(final_text_data)
